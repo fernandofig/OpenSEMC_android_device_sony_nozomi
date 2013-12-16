51a6a917b336c121c42eac4bd7f04e674852a10f
fernandofig <fernando.fig@gmail.com>
2013-12-16 15:18:47 -0200

    Adjust build desc and fingerprint to match Sony's 6.2.B.1.96 firmware

--- cm.mk
+++ cm.mk
@@ -32,7 +32,7 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-6921 BUILD_FINGERPRINT=SEMC/LT26i_1257-6921/LT26i:4.1.2/6.2.B.1.96/n7v_zg:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.1.2 6.2.B.1.96 n7v_zg test-keys"
 
 PRODUCT_NAME := cm_nozomi
 PRODUCT_DEVICE := nozomi
